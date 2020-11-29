36c2a70a2b1ad5c15a9c335b4a8d5da9735bd90c
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2020-11-29 18:09:27 +0530

    improved the script

--- script.js
+++ script.js
@@ -1,79 +1,98 @@
-const axios = require('axios');
+const axios = require("axios");
+require("dotenv").config();
 
-const addOrUpdateMembershipInOrg = async (userid) => {
-	try {
-		const response = await axios({
-			method: 'put',
-			url: `https://api.github.com/orgs/masaiTest/teams/cohort-test/memberships/${userid}`,
-			headers: {
-				Authorization: 'Bearer 34791a5a9fff5d74dca70f788c3b32c97a99da80'
-			}
-		});
-		return response;
-	} catch (error) {
-		return error;
-	}
+const authToken = `Bearer ${process.env.AUTH_TOKEN}`;
+const org = "masaiTest"; // organization name
+const team = "cohort-test"; // team name
+const owner = "masaiTest";
+const templateRepo = "template-repo"; // repository template to use
+const repoNames = ["student-7", "student-8", "student-9"];
+const userNames = ["falakthkr", "keshavmahawar", "manideepTest"];
+
+const addOrUpdateMembershipInOrg = async (username) => {
+  try {
+    const response = await axios({
+      method: "put",
+      url: `https://api.github.com/orgs/${org}/teams/${team}/memberships/${username}`,
+      headers: {
+        Authorization: authToken,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 const addMembers = async () => {
-	const usernames = [ 'falakthkr', 'keshavmahawar', 'manideepTest' ];
-	const status = await Promise.all(usernames.map((username) => addOrUpdateMembershipInOrg(username)));
-	console.log('Status =>', status);
+  const status = await Promise.all(
+    userNames.map((username) => addOrUpdateMembershipInOrg(username))
+  );
+  console.log("Status =>", status);
 };
 
-const createUsingTemplate = async (template_owner, template_repo, repo_name) => {
-	try {
-		const response = await axios({
-			method: 'post',
-			url: `https://api.github.com/repos/${template_owner}/${template_repo}/generate`,
-			headers: {
-				Authorization: 'Bearer 34791a5a9fff5d74dca70f788c3b32c97a99da80',
-				Accept: 'application/vnd.github.baptiste-preview+json'
-			},
-			data: {
-				name: `${repo_name}`,
-				owner: `${template_owner}`,
-				private: true
-			}
-		});
-		return response;
-	} catch (error) {
-		return error;
-	}
+const createUsingTemplate = async (repositoryName) => {
+  try {
+    const response = await axios({
+      method: "post",
+      url: `https://api.github.com/repos/${owner}/${templateRepo}/generate`,
+      headers: {
+        Authorization: authToken,
+        Accept: "application/vnd.github.baptiste-preview+json",
+      },
+      data: {
+        name: `${repositoryName}`,
+        owner: `${owner}`,
+        private: true,
+      },
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 const createRepositories = async () => {
-	const repoNames = [ 'student-five', 'student-six', 'student-seven' ];
-	const status = await Promise.all(repoNames.map((name) => createUsingTemplate('masaiTest', 'template-repo', name)));
-	console.log('Status =>', status);
+  const status = await Promise.all(
+    repoNames.map((name) => createUsingTemplate(name))
+  );
+  console.log("Status =>", status);
 };
 
-const addCollaborator = async (owner, repo, username) => {
-	try {
-		const response = await axios({
-			method: 'put',
-			url: `https://api.github.com/repos/${owner}/${repo}/collaborators/${username}`,
-			headers: {
-				Authorization: 'Bearer 34791a5a9fff5d74dca70f788c3b32c97a99da80',
-				Accept: 'application/vnd.github.baptiste-preview+json'
-			},
-			data: {
-				permission : "admin"
-			}
-		});
-		return response;
-	} catch (error) {
-		return error;
-	}
+const addCollaborator = async (repo, username) => {
+  try {
+    const response = await axios({
+      method: "put",
+      url: `https://api.github.com/repos/${owner}/${repo}/collaborators/${username}`,
+      headers: {
+        Authorization: authToken,
+        Accept: "application/vnd.github.baptiste-preview+json",
+      },
+      data: {
+        permission: "admin",
+      },
+    });
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
 
 const addCollaboratorsToRepositories = async () => {
-	const repoNames = [ 'student-five', 'student-six', 'student-seven' ];
-	const usernames = [ 'falakthkr', 'keshavmahawar', 'manideepTest' ];
-	const status = await Promise.all(usernames.map((name, i) => addCollaborator('masaiTest', repoNames[i], name)));
-	console.log('Status =>', status);
+  const status = await Promise.all(
+    userNames.map((name, i) => addCollaborator(repoNames[i], name))
+  );
+  console.log("Status =>", status);
 };
 
-// console.log(addMembers())
-// console.log(createRepositories())
-// console.log(addCollaboratorsToRepositories())
+async function automate() {
+  try {
+    await addMembers();
+    await createRepositories();
+    await addCollaboratorsToRepositories();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(automate());
